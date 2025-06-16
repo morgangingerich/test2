d161ea91348fffc458bcba8f1899f37054df0bbc
Morgan Laptop <mgingerich@edwardsaquifer.org>
2025-06-16 10:47:25 -0500

    fixing columns

--- friday_practice.R
+++ friday_practice.R
@@ -1,11 +1,10 @@
 library("readr")
 library("tidyverse")
-example<-read_csv("may_25_example1.csv")
-read_csv(
-  "may_25_example1.csv",
-  col_types = cols(
-    date = col_character(),
-    val = col_double(),
-    site = col_factor(levels =c("J 17","J 27","Comal","San Marcos"))
-  )
-)
+
+example<-read_csv("may_25_example1.csv", col_types = cols(
+  .default = "d",  # Set default to double
+  date = col_date("%m/%d/%Y"), # Specify date column
+  val = col_double(),
+  site = col_factor()
+))
+example
